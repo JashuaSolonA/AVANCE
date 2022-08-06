2f9b8b9e76f14346a01f1c921bbb6de0b779a570
jeremycalderon <t1023600121@uitru.edu.pe>
2022-08-06 17:03:09 -0500

    revisa

---
+++ mariobros.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+pygame.mixer.init(frequency=44100)
+
+pygame.mixer.music.load("do.wav")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(1.0)
+
+size = 800, 600
+
+VENTANA = pygame.display.set_mode(size)
+pygame.display.set_caption('Musica')
+Imagen = pygame.image.load('recursos/LA.jpg')
+pygame.display.set_icon(Imagen)
+
+fondo = 'do.png'
+fondo_original = pygame.image.load(fondo)
+fondo_redimensionada = pygame.transform.scale(fondo_original, size)
+
+VENTANA.blit(fondo_redimensionada,(0,0))
+
+ejecuta = True
+
+while ejecuta:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ejecuta = False
+    
+    keys=pygame.key.get_pressed()
+
+    if keys[pygame.K_2]:
+        pygame.mixer.music.set_volume(0.25)
+
+    elif keys[pygame.K_3]:
+        pygame.mixer.music.set_volume(0.50)
+    
+    elif keys[pygame.K_1]:
+        pygame.mixer.music.set_volume(0.0)
+
+    elif keys[pygame.K_4]:
+        pygame.mixer.music.set_volume(1.0)
+
+pygame.quit()
