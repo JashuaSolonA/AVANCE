02e8042f47f0e507ffec422bb293132b98e64026
jeremycalderon <t1023600121@uitru.edu.pe>
2022-08-06 17:23:23 -0500

    revisa

--- mariobros.py
+++ mariobros.py
@@ -3,7 +3,7 @@ import sys
 
 pygame.mixer.init(frequency=44100)
 
-pygame.mixer.music.load("do.wav")
+pygame.mixer.music.load("ringtones-super-mario-bros.mp3")
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(1.0)
 
@@ -11,10 +11,10 @@ size = 800, 600
 
 VENTANA = pygame.display.set_mode(size)
 pygame.display.set_caption('Musica')
-Imagen = pygame.image.load('recursos/LA.jpg')
+Imagen = pygame.image.load('mariobros.png')
 pygame.display.set_icon(Imagen)
 
-fondo = 'do.png'
+fondo = 'mariobros.png'
 fondo_original = pygame.image.load(fondo)
 fondo_redimensionada = pygame.transform.scale(fondo_original, size)
 
