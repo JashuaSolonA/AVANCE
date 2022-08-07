5755706a4c4ec4a871af35b6d43bacc03cfd4527
AbigailBCarrillo <106496635+AbigailBCarrillo@users.noreply.github.com>
2022-08-06 23:45:56 -0500

    Update Interfaz_PYSIMPLEGUI.py

--- Interfaz_PYSIMPLEGUI.py
+++ Interfaz_PYSIMPLEGUI.py
@@ -2,77 +2,90 @@ import PySimpleGUI as sg #Importación
 
 sg.theme("DarkBlue8") # Diseño de la ventana 
 
-# Funciones a utilizar
-def menu_inicio(): #En progreso, aún no se usa
-    event, values = inicio.read()
-    if event == "Aprende las notas musicales":
-        inicio.close()
-        while True:
-            event, values = aprender.read()
-            if event == sg.WIN_CLOSED:
-                break
-            if event == "Regresar al menú inicio": 
-                event, values = inicio.read()
-                aprender.close()
-            if event == "Pon en práctica tus conocimientos":
-                inicio.close()
-                while True:
-                    event, values = elegir.read()
-                    if event == "Salir de la aplicación" or event == sg.WIN_CLOSED:
-                        break
-                    if event == "Regresar al menú inicio":
-                        event, values = inicio.read()
-                    if event == "sol":
-                        elegir.close()
-                        while True:
-                            event, values = juego_sol.read()
-                            if event == sg.WIN_CLOSED:
-                                break
-                    elif event =="fa":
-                        elegir.close()
-                        while True:
-                            event, values = juego_fa.read()
-                            if event == sg.WIN_CLOSED:
-                                break
-
-
-# Contenidos de las ventanas
-ly_saludo = [  [sg.Text("Aprende a leer el pentagrama", size =(30, 2))],
-            [sg.Text("Introduce tu nombre: ", size =(15, 1)), sg.Input()],
+# Funciones para crear las ventanas
+def hacer_saludo():
+    ly_saludo = [  [sg.Text("Aprende a leer el pentagrama")],
+            [sg.Text("Introduce tu nombre: "), sg.Input()],
             [sg.Button("OK")], [sg.Button("Salir")]
             ]
+    return sg.Window("Proyecto Grupo 08: Aprende a leer el pentagrama", ly_saludo)
 
-ly_inicio = [  [sg.Button("Aprende las notas musicales")],
+def hacer_inicio():
+    ly_inicio = [  [sg.Button("Aprende las notas musicales")],
             [sg.Button("Pon en práctica tus conocimientos")],
             [sg.Button("Salir de la aplicación")]
             ]
+    return sg.Window("Menú inicio", ly_inicio)
 
-ly_aprender = [ [sg.Text("Mostrar el pentagrama con las notas musicales ubicadas, tanto en clave de sol como de fa")],
-            [sg.Button("Regresar al menú inicio")]
-            ]
-
-ly_elegir = [  [sg.Text("Elige la clave de tu preferencia")],
-            [sg.Button("sol")],
-            [sg.Button("fa")],
-            [sg.Button("Regresar al menú inicio")],
+def hacer_aprender():
+    ly_aprender = [ [sg.Text("Mostrar el pentagrama con las notas musicales ubicadas, tanto en clave de sol como de fa")],
+            [sg.Image("clave_de_sol.png")],
+            [sg.Image("clave_de_fa.png")],
             [sg.Button("Salir de la aplicación")]
             ]
+    return sg.Window("Aprende la ubicación de las notas musicales", ly_aprender)
 
-ly_sol = [
-            [sg.Text("Practica con un pentagrama en clave de sol", size =(30, 2))]
-        ]
+def hacer_elegir():
+    ly_elegir = [  [sg.Text("Elige la clave de tu preferencia")],
+            [sg.Button("sol")],
+            [sg.Button("fa")],
+            [sg.Button("Salir de la aplicación")]
+            ]
+    return sg.Window("Elige la clave", ly_elegir)
 
-ly_fa = [
-            [sg.Text("Practica con un pentagrama en clave de fa", size =(30, 2))]
+def hacer_juego_sol():
+    ly_sol = [
+            [sg.Text("Practica con un pentagrama en clave de sol")]
         ]
+    return sg.Window("Juego en clave de sol", ly_sol)
+
+def hacer_juego_fa():
+    ly_fa = [
+            [sg.Text("Practica con un pentagrama en clave de fa")]
+        ]
+    return sg.Window("Juego en clave de fa", ly_fa)
 
 # Creación de las ventanas
-saludo = sg.Window("Proyecto Grupo 08: Aprende a leer el pentagrama", ly_saludo)
-inicio = sg.Window("Menú inicio", ly_inicio)
-elegir = sg.Window("Elige la clave", ly_elegir)
-aprender = sg.Window("Aprende la ubicación de las notas musicales", ly_aprender)
-juego_sol = sg.Window("Juego en clave de sol", ly_sol)
-juego_fa = sg.Window("Juego en clave de fa", ly_fa)
+saludo = hacer_saludo()
+inicio = hacer_inicio()
+aprender = hacer_aprender()
+elegir = hacer_elegir()
+juego_sol = hacer_juego_sol()
+juego_fa = hacer_juego_fa()
+
+#Def
+def menu_inicio():
+    while True:
+        event, values = inicio.read()
+        if event == "Salir de la aplicación"  or event == sg.WIN_CLOSED:
+            break
+        if event == "Aprende las notas musicales":
+            inicio.close()
+            while True:
+                event, values = aprender.read()
+                if event == "Salir de la aplicación" or event == sg.WIN_CLOSED:
+                    break
+        elif event == "Pon en práctica tus conocimientos":
+            inicio.close()
+            while True:
+                event, values = elegir.read()
+                if event == "Salir de la aplicación" or event == sg.WIN_CLOSED:
+                    break
+                elif event == "sol":
+                    elegir.close()
+                    while True:
+                        event, values = juego_sol.read()
+                        if event == sg.WIN_CLOSED:
+                            break
+                elif event =="fa":
+                    elegir.close()
+                    while True:
+                        event, values = juego_fa.read()
+                        if event == sg.WIN_CLOSED:
+                            break
+
+def a_inicio():
+    return menu_inicio()
 
 try:
     while True:
@@ -80,7 +93,7 @@ try:
         if event == "Salir"  or event == sg.WIN_CLOSED:
             break
         if event == "OK":
-            sg.popup_ok("Bienvenido " + values[0] + ". ¿Estás listo para empezar a aprender?")
+            sg.popup_ok("Bienvenido/a " + values[0] + ". ¿Estás listo/a para empezar a aprender?")
             saludo.close()
             while True:
                 event, values = inicio.read()
@@ -90,19 +103,22 @@ try:
                     inicio.close()
                     while True:
                         event, values = aprender.read()
-                        if event == sg.WIN_CLOSED:
+                        if event == "Salir de la aplicación" or event == sg.WIN_CLOSED:
                             break
-                        if event == "Regresar al menú inicio": 
-                            event, values = inicio.read()
+                        if event == "Regresar al menú inicio":
                             aprender.close()
-                if event == "Pon en práctica tus conocimientos":
+                            inicio = hacer_inicio() 
+                            event, values = inicio.read()
+                elif event == "Pon en práctica tus conocimientos":
                     inicio.close()
                     while True:
                         event, values = elegir.read()
                         if event == "Salir de la aplicación" or event == sg.WIN_CLOSED:
                             break
                         if event == "Regresar al menú inicio":
-                            event, values = inicio.read()
+                            elegir.close()
+                            inicio = hacer_inicio()
+                            x = a_inicio()
                         if event == "sol":
                             elegir.close()
                             while True:
@@ -120,6 +136,8 @@ except:
 finally:
     saludo.close()
     inicio.close()
+    aprender.close()
     elegir.close()
     juego_sol.close()
     juego_fa.close()
+    print("Todo cerrado")
